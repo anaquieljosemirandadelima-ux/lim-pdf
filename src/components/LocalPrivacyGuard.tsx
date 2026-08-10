0ca801f970a6eeedea80f516b3e910bb1b9ca7da
anaquieljosemirandadelima-ux <anaquieljosemirandadelima@gmail.com>
2026-08-10 16:40:32 -0300

    fix: clean expired local data automatically

---
+++ src/components/LocalPrivacyGuard.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
+import { cleanupExpiredEditorDrafts } from "@/lib/editor-drafts";
+import { getTemporaryCacheStatus } from "@/lib/temporary-cache";
+
+const CLEANUP_INTERVAL_MS = 5 * 60 * 1000;
+
+export function LocalPrivacyGuard() {
+  useEffect(() => {
+    const cleanup = () => {
+      cleanupExpiredEditorDrafts();
+      void getTemporaryCacheStatus().catch(() => undefined);
+    };
+
+    cleanup();
+    const interval = window.setInterval(cleanup, CLEANUP_INTERVAL_MS);
+    window.addEventListener("storage", cleanup);
+    window.addEventListener("visibilitychange", cleanup);
+    return () => {
+      window.clearInterval(interval);
+      window.removeEventListener("storage", cleanup);
+      window.removeEventListener("visibilitychange", cleanup);
+    };
+  }, []);
+
+  return null;
+}

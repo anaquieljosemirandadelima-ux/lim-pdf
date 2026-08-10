713cd389a1be0384f647f0fdb7fffbd0fbc4d0e5
anaquieljosemirandadelima-ux <anaquieljosemirandadelima@gmail.com>
2026-08-10 20:13:36 -0300

    Corrige ícones da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
                 <LucideClientIcon name="Sparkles" size={18} /> Explorar ferramentas
               </Link>
               <Link className="reference-ghost-button" href="/ferramentas/editar-pdf">
-                <span className="play-dot"><LucideClientIcon name="Play" size={14} /></span> Ver como funciona
+                <span className="play-dot"><LucideClientIcon name="ArrowRight" size={14} /></span> Ver como funciona
               </Link>
             </div>
           </div>
@@ -60,7 +60,7 @@ export default function Home() {
               <div className="doc-line wide" /><div className="doc-line" /><div className="doc-line medium" /><div className="doc-line small" />
               <span className="doc-brand">L</span>
             </div>
-            <span className="floating-card convert"><LucideClientIcon name="RefreshCw" size={25} /><b>Converter</b></span>
+            <span className="floating-card convert"><LucideClientIcon name="Repeat2" size={25} /><b>Converter</b></span>
             <span className="floating-card edit"><LucideClientIcon name="PencilLine" size={25} /><b>Editar</b></span>
             <span className="floating-card sign"><LucideClientIcon name="Signature" size={25} /><b>Assinar</b></span>
             <span className="floating-card protect"><LucideClientIcon name="ShieldCheck" size={25} /><b>Proteger</b></span>
